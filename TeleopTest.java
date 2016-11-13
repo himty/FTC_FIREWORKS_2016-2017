520b287cf034b3c26aa14b0c003652919370e1bb
himty <jenny.wang244@gmail.com>
2016-11-13 12:20:41 -0800

    Add linear slide

--- TeleopTest.java
+++ TeleopTest.java
@@ -1,23 +1,17 @@
 /*
 Copyright (c) 2016 Robert Atkinson
-
 All rights reserved.
-
 Redistribution and use in source and binary forms, with or without modification,
 are permitted (subject to the limitations in the disclaimer below) provided that
 the following conditions are met:
-
 Redistributions of source code must retain the above copyright notice, this list
 of conditions and the following disclaimer.
-
 Redistributions in binary form must reproduce the above copyright notice, this
 list of conditions and the following disclaimer in the documentation and/or
 other materials provided with the distribution.
-
 Neither the name of Robert Atkinson nor the names of his contributors may be used to
 endorse or promote products derived from this software without specific prior
 written permission.
-
 NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
 LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
@@ -38,6 +32,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
+
+
 /**
  * This OpMode uses the common HardwareK9bot class to define the devices on the robot.
  * All device access is managed through the HardwareK9bot class. (See this class for device names)
@@ -55,7 +51,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="TeleopTest", group="K9bot")
+@TeleOp(name="TeleopTestLinear", group="K9bot")
 public class TeleopTest extends LinearOpMode {
 
     public enum servoStatus  {
@@ -90,6 +86,7 @@ public class TeleopTest extends LinearOpMode {
 
         double leftPower;
         double rightPower;
+        double linearPower;
 
         /* Initialize the hardware variables.
          * The init() method of the hardware class does all the work here
@@ -110,16 +107,19 @@ public class TeleopTest extends LinearOpMode {
             //drive train
             leftPower = (float) scaleInput(gamepad1.right_stick_y - gamepad1.right_stick_x);
             rightPower = (float) scaleInput(gamepad1.right_stick_y + gamepad1.right_stick_x);
+            linearPower = (float) scaleInput(-1*gamepad2.left_stick_y);
+            
             if (gamepad1.right_bumper){
                 leftPower /= 5;
                 rightPower /= 5;
             }
             robot.leftMotor.setPower((double) -1*leftPower);
             robot.rightMotor.setPower((double) -1 * rightPower);
-            telemetry.addData("Powers", "Left: " + String.format("%.2f", leftPower) + " " + "Right: " + String.format("%.2f", rightPower));
+            //robot.linearSlide.setPower((double) -1 * linearPower);
 
+            telemetry.addData("Powers", "Left: " + String.format("%.2f", leftPower) + " " + "Right: " + String.format("%.2f", rightPower));
             //linear slide
-//            robot.linearSlide.setPower((double)scaleInput(gamepad1.left_stick_y));
+            robot.linearSlide.setPower((double)linearPower);
 
             //arm movement
             if (gamepad1.a && armStatus.equals(servoStatus.HOLD)){
